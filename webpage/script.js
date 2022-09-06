8a1dbcd060e2f4cf47c8f69eaa1c9bbb3a429b94
Stefan Danzl <stefan.danzl@live.de>
2022-09-06 10:29:13 +0200

    webworker internal

--- webpage/script.js
+++ webpage/script.js
@@ -252,48 +252,7 @@ function fokus(){
 
 
 
-let worker = new Worker(
-    `data:text/javascript,
-
-    
-    var doUpdate = false;
-
-
-    function checkChange(){
-
-        console.log("CHECK CHANGE");
-
-        if (document.querySelector('#todos').innerText != prev_todos){doUpdate = true;}
-
-        if (document.querySelector('#todos').innerText != prev_text){doUpdate = true;}
-        
-        if (document.querySelector('#calendar').innerText != prev_calendar){doUpdate = true;}
-        
-        if (doUpdate){
-            save();
-            update();
-            doUpdate = false;
-        }
-
-
-    }
-
-
-    function timer(){
-        checkChange();
-        setInterval(timer,5000);
-
-    }
-    onmessage = function(){    //This will be called when worker.postMessage is called in the outside code.
-        
-        timer();
-        
-        //let foo = event.data;    //Get the argument that was passed from the outside code, in this case foo.
-        //let result = functionThatTakesLongTime(foo);    //Find the result. This will take long time but it doesn't matter since it's called in the worker.
-        //postMessage(result);    //Send the result to the outside code.
-    };
-    `
-);
+let worker = new Worker("worker.js");
 
 // worker.onmessage = function(event){    //Get the result from the worker. This code will be called when postMessage is called in the worker.
 //     alert("The result is " + event.data);
